ad9fb20a80028eeae7b3582ef759fadfd5782e82
Anay Chopade <shrichopade@gmail.com>
2021-01-12 21:46:58 +0000

    added auth and backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
@@ -7,9 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello, welcome to the ReactApp deployed on AWS Amplify.</h1>
+		<h2>Auth is now added to the application!!!</h2>
       </header>
+	   <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
